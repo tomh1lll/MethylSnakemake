85c0b76c6a6e073835b08e4a71755855f14f565f
Tom Hill <tom.hill@nih.gov>
2022-02-16 07:35:11 -0500

    Update bsseq_lm.R

--- scripts/bsseq_lm.R
+++ scripts/bsseq_lm.R
@@ -56,21 +56,32 @@ pheno2<-as.data.frame(pheno[,2])
 head(pheno2)
 
 ###exclude low coverage
-cov_frac = as.character(as.numeric(args[5]))
-cov_level = as.character(as.numeric(args[6]))
+cov_frac = as.numeric(as.character(args[4]))
+cov_level = as.numeric(as.character(args[5]))
 sample_min = min(table(pheno$Dx))
 cov_cutoff = ceiling(sample_min*cov_frac)
 
 chr1_cov = getCoverage(chr1, type="Cov", what="perBase")
+print("chr1_cov done")
 keepLoci.ex = which(rowSums(chr1_cov  >= cov_level) >= cov_cutoff)
+print("keepLoci done") 
 chr1_filtered = chr1[keepLoci.ex,]
+print("chr1_filtered done") 
 
 chr1_betavals_df = data.frame(getMeth(chr1_filtered, type="raw"))
+print("chr1_betavals done") 
 chr1_granges = granges(chr1_filtered)
 chr1_granges_df = data.frame(chromosome=seqnames(chr1_granges),startpos=start(chr1_granges),endpos=end(chr1_granges),strand=strand(chr1_granges))
 chr1_betavals_granges_df = data.frame(chr1_granges_df, chr1_betavals_df, row.names=paste(chr1_granges_df$chromosome,chr1_granges_df$startpos,sep="_"), check.names=F)
 
-new<-data.frame(t(chr1_betavals_granges_df[,5:dim(chr1_betavals_granges_df)[2]]),pheno[,-c(1)])
+chr1_betavals_granges_df2 = na.omit(chr1_betavals_granges_df)
+
+keep <- apply(chr1_betavals_granges_df2[,c(-1,-2,-3,-4)], 1, function(x) length(unique(x[!is.na(x)])) != 1)
+chr1_betavals_granges_df3 = chr1_betavals_granges_df2[keep, ]
+
+print(dim(chr1_betavals_granges_df3))
+
+new<-data.frame(t(chr1_betavals_granges_df3[,5:dim(chr1_betavals_granges_df3)[2]]),pheno[,-c(1)])
 ncolu<-dim(new)[2]
 names(new)[ncolu]<-"Dx"
 chr1_betavals_granges_df2 = as_tibble(new, rownames="Library_ID")
@@ -79,13 +90,16 @@ chr1_betavals_granges_df3 = pivot_longer(chr1_betavals_granges_df2, cols=starts_
 
 chr1_betavals_granges_df4 = chr1_betavals_granges_df3 %>% group_by(CpGID_chr_bp) %>% nest()
 
+print("create dataframe")
+head(chr1_betavals_granges_df4)
+
 mod_fun <- function(df) lm(methylation ~ factor(Dx), data=df)
 
 chr1_lm_results = chr1_betavals_granges_df4 %>% mutate(model = map(data,mod_fun))
 
 chr1_lm_results2 = chr1_lm_results %>% group_by(CpGID_chr_bp) %>% transmute(beta = map_dfr(model,tidy)$estimate[2], pvalue = map_dfr(model,tidy)$p.value[2])
 
-write.table(chr1_lm_results2, file=args[3], sep = "\t", row.names=FALSE)
+print("model fit complete")
 
 chr1_lm_results3 = as.data.frame(do.call(rbind, strsplit(chr1_lm_results2$CpGID_chr_bp,split = "_")))
 chr1_lm_results3$V2 = as.numeric(as.character(chr1_lm_results3$V2))
@@ -96,4 +110,4 @@ colnames(chr1_lm_results3) = c("chr","start","end","p","beta")
 
 chr1_lm_results4 = chr1_lm_results3[chr1_lm_results3$p != "NaN",]
 
-write.table(chr1_lm_results4, file=args[4], sep = "\t", row.names=FALSE,quote=F)
+write.table(chr1_lm_results4, file=args[3], sep = "\t", row.names=FALSE,quote=F)
